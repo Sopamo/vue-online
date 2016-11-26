19c97aaddc6ef0a0255a2de327ccbd2a4b970349
Paul Mohr <github@sopamo.de>
2016-11-26 23:35:31 +0100

    Cleanup

---
+++ build.js
@@ -0,0 +1,19 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.ConnectionStatus = exports.OfflineIndicator = undefined;
+
+var _OfflineIndicator = require('./src/components/OfflineIndicator');
+
+var _OfflineIndicator2 = _interopRequireDefault(_OfflineIndicator);
+
+var _ConnectionStatus = require('./src/ConnectionStatus');
+
+var _ConnectionStatus2 = _interopRequireDefault(_ConnectionStatus);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.OfflineIndicator = _OfflineIndicator2.default;
+exports.ConnectionStatus = _ConnectionStatus2.default;
--- webpack.config.build.js
+++
@@ -1,9 +0,0 @@
-var webpack = require('webpack')
-module.exports = require('./webpack.config.js')
-
-module.exports.entry = {
-    'vue-online': './main.js'
-}
-
-module.exports.output.library = 'VueOnlineComponent';
-module.exports.output.libraryTarget = 'umd';
